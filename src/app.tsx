1a056a660cba9232eb4e18ddfdba26dd91eeef54
yermie123 <yermolovpersonal@gmail.com>
2024-12-18 22:24:16 -0800

    First commit

---
+++ src/app.tsx
@@ -0,0 +1,21 @@
+import { Router } from "@solidjs/router";
+import { FileRoutes } from "@solidjs/start/router";
+import { Suspense } from "solid-js";
+import Nav from "~/components/Nav";
+import "./app.css";
+import "@fontsource/inter";
+
+export default function App() {
+  return (
+    <Router
+      root={(props) => (
+        <>
+          <Nav />
+          <Suspense>{props.children}</Suspense>
+        </>
+      )}
+    >
+      <FileRoutes />
+    </Router>
+  );
+}
