c16a32c13677bebd2958268232e8901a53c54c47
yermie123 <yermolovpersonal@gmail.com>
2024-12-29 20:31:12 -0800

    Adjusting interaction between signals/stores and inputs

--- src/components/game/GamePrep.tsx
+++ src/components/game/GamePrep.tsx
@@ -1,16 +1,17 @@
 import { For, createSignal, onMount, Show, createEffect } from "solid-js";
+import { createStore } from "solid-js/store";
 import type { Component } from "solid-js";
 
 const GamePrep: Component<{
   gameState: () => any;
   gameStateSet: (value: any) => void;
 }> = (props) => {
-  const [team1Values, team1ValuesSet] = createSignal<any>([]);
-  const [team2Values, team2ValuesSet] = createSignal<any>([]);
+  const [team1Values, team1ValuesSet] = createStore<any>([]);
+  const [team2Values, team2ValuesSet] = createStore<any>([]);
 
   createEffect(() => {
-    console.log("team1: ", team1Values());
-    console.log("team2: ", team2Values());
+    console.log("team1: ", team1Values);
+    console.log("team2: ", team2Values);
   });
 
   return (
@@ -20,11 +21,21 @@ const GamePrep: Component<{
       <div id="teams">
         <div id="team1">
           <h2>Team 1</h2>
-          <PlayerInput team={1} tVSet={team1ValuesSet} tV={team1Values} />
+          <PlayerInput
+            team={1}
+            tVSet={team1ValuesSet}
+            tV={team1Values}
+            otherTeamSet={team2ValuesSet}
+          />
         </div>
         <div id="team2">
           <h2>Team 2</h2>
-          <PlayerInput team={2} tVSet={team2ValuesSet} tV={team2Values} />
+          <PlayerInput
+            team={2}
+            tVSet={team2ValuesSet}
+            tV={team2Values}
+            otherTeamSet={team1ValuesSet}
+          />
         </div>
       </div>
       <button class="success" onClick={() => alert("TODO: Finalize")}>
@@ -36,19 +47,33 @@ const GamePrep: Component<{
 
 const PlayerInput: Component<{
   team: number;
-  tV: () => any;
-  tVSet: (value: any) => void;
+  tV: any;
+  tVSet: any;
+  otherTeamSet: any;
 }> = (props) => {
+  const combineWithSignal = (value: string, index: number) => {
+    console.log("Value: ", value);
+    props.tVSet([index], value);
+    console.log("Signal: ", props.tV);
+  };
+
   const reloadRemove = (index: string) => {
-    let tempArr = [...props.tV()];
+    let tempArr = [...props.tV];
     tempArr.splice(parseInt(index, 10), 1);
     props.tVSet(tempArr);
     console.log("Removed: ", tempArr);
   };
 
+  const playerToOtherTeam = (index: string) => {
+    let tempArr = [...props.tV];
+    let tempPlayer = tempArr.splice(parseInt(index, 10), 1);
+    props.otherTeamSet((prev: any) => [...prev, tempPlayer]);
+    props.tVSet(tempArr);
+  };
+
   return (
     <>
-      <For each={props.tV()}>
+      <For each={props.tV}>
         {(player: any, index: any) => (
           <div class="player-input">
             <Show
@@ -57,13 +82,17 @@ const PlayerInput: Component<{
                 <img src={"/trash.svg"} onClick={() => reloadRemove(index())} />
               }
             >
-              <img src={"/arrow-icons/arrow-left.svg"} />
+              <img
+                src={"/arrow-icons/arrow-left.svg"}
+                onClick={() => playerToOtherTeam(index())}
+              />
             </Show>
             <input
               type="text"
               class={`player-${props.team}`}
               placeholder={`Player ${index() + 1}`}
-              onInput={(e) => e.target.value}
+              onInput={(e) => combineWithSignal(e.currentTarget.value, index())}
+              innerText={player !== "" ? player : ""}
             />
             <Show
               when={props.team === 1}
@@ -71,7 +100,10 @@ const PlayerInput: Component<{
                 <img src={"/trash.svg"} onClick={() => reloadRemove(index())} />
               }
             >
-              <img src={"/arrow-icons/arrow-right.svg"} />
+              <img
+                src={"/arrow-icons/arrow-right.svg"}
+                onClick={() => playerToOtherTeam(index())}
+              />
             </Show>
           </div>
         )}
